c6b35c887e579373515b20d5c47de1859a519f91
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-01-29 15:58:32 -0300

    Manejor de archivos

--- files/files.go
+++ files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"bufio"
 	"fmt"
 	"golang/teclado"
 	"os"
@@ -8,7 +9,8 @@ import (
 
 func GrabaTabla() {
 	texto := teclado.TablaMultiplicar()
-	archivo, err := os.Create("files/tabla.txt")
+	filename := "files/tabla.txt"
+	archivo, err := os.Create(filename)
 
 	if err != nil {
 		fmt.Print("Hubo un error")
@@ -20,4 +22,43 @@ func GrabaTabla() {
 
 func SumaTabla() {
 
+	var texto string = teclado.TablaMultiplicar()
+	filename := "files/tabla.txt"
+	if !append(filename, texto) {
+		fmt.Println("Error al concatenar texto")
+	}
+}
+
+func append(filename string, texto string) bool {
+	archivo, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println("Hubo un error")
+		return false
+	}
+	_, err = archivo.WriteString(texto)
+
+	if err != nil {
+		fmt.Println("Hubo un error")
+		return false
+	}
+	archivo.Close()
+	return true
+}
+
+func LeerArchivo() {
+	archivo, err := os.Open("files/tabla.txt")
+	if err != nil {
+		fmt.Println("Hubo un error")
+		return
+	}
+	archivo.Close()
+
+	scanner := bufio.NewScanner(archivo)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Hubo un error")
+	}
 }
--- main.go
+++ main.go
@@ -36,4 +36,6 @@ func main() {
 
 	//teclado.TablaMultiplicar()
 	files.GrabaTabla()
+	files.SumaTabla()
+	files.LeerArchivo()
 }
